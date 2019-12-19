f2f041a77a0ccb9ba574886de7ade62bdfad557e
alan <alan.fernandes63@gmail.com>
2019-12-19 09:35:02 -0300

    passando parametros via post

--- user_recource.py
+++ user_recource.py
@@ -1,7 +1,18 @@
-from flask_restful import Resource
+from flask_restful import Resource,reqparse
+
 
 class User_Resource(Resource):
+    parse = None
+
+    def __init__(self):
+        self.parse = reqparse.RequestParser()
+        self.parse.add_argument('user', location='json')
+
     def get(self):
         return "Hello word"
+    def post(self):
+
+        args = self.parse.parse_args()
+        print(args)
 
 
