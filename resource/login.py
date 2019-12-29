503833638b8b354004006f92933d83e82511582c
alan <alan.fernandes63@gmail.com>
2019-12-29 11:31:22 -0300

    convertendo a senha em hash e verificando o match no login

--- resource/login.py
+++ resource/login.py
@@ -1,8 +1,11 @@
 from flask_jwt_extended import create_access_token, create_refresh_token
 from flask_restplus import Resource,reqparse
 from flask_restplus import Resource
+from model.user import User
+from mongoengine.errors import DoesNotExist
 from flask import jsonify
 import datetime
+import bcrypt
 import json
 
 
@@ -16,13 +19,28 @@ class Login(Resource):
         user_json = json.dumps(eval(args['user']))
 
         user_dictionary = json.loads(user_json)
-        username = user_dictionary['username']
-        password = user_dictionary['password']
+        if 'username' in user_dictionary and 'password' in user_dictionary:
+            username = user_dictionary['username']
+            password = user_dictionary['password']
+
+            try:
+                user = User.objects(name=username).get()
+            except DoesNotExist:
+                return "user not found", 404
+
+            # checks if the password coming from the request is the same as the one in the database
+            if bcrypt.checkpw(bytes(password,'utf-8'), user['password']):
+                expires = datetime.timedelta(days=360)
+                token = create_refresh_token(identity=username, expires_delta=expires)
+                return token, 200
+            else:
+                return "senha incorreta", 404
+        else:
+            return "invalid payload", 400
+
 
         #ret = {'access_token': create_access_token('alan')}
         #return jsonify(ret), 200
-        #expires = datetime.timedelta(days=1)
         #token = create_access_token(identity='alan', expires_delta=expires)
         #token = create_refresh_token(identity='alan', expires_delta=expires)
         #return token
-        return ""
